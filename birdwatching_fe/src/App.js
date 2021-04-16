c34bdccd015d6e76fb6d309b369afc640a0c48e2
Kaushik Guha <kguha@mail.sjsm.org>
2021-04-16 00:15:46 -0500

    4-16 After cleanup NewSession.js

--- birdwatching_fe/src/App.js
+++ birdwatching_fe/src/App.js
@@ -1,87 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Axios from "axios";
+import React, { Component } from 'react';
+import NewUser from './components/NewUser.js';
+import UpdateUser from './components/UpdateUser.js';
+import DeleteUser from './components/DeleteUser.js';
+import JournalUser from './components/JournalUser.js';
+import ShowUsers from './components/ShowUsers.js';
+import ShowSingleUser from './components/ShowSingleUser.js';
+import JournalShow from './components/JournalShow.js';
+import JournalEdit from './components/JournalEdit.js';
+let baseURL = '';
 
-function App() {
-  return (
-    <div className="App">
-    function App() {
-const [registerUsername, setRegisterUsername] = useState("");
-const [registerPassword, setRegisterPassword] = useState("");
-const [loginUsername, setLoginUsername] = useState("");
-const [loginPassword, setLoginPassword] = useState("");
-const [data, setData] = useState(null);
-const register = () => {
-  Axios({
-    method: "POST",
-    data: {
-      username: registerUsername,
-      password: registerPassword,
-    },
-    withCredentials: true,
-    url: "http://localhost:4000/register",
-  }).then((res) => console.log(res));
-};
-const login = () => {
-  Axios({
-    method: "POST",
-    data: {
-      username: loginUsername,
-      password: loginPassword,
-    },
-    withCredentials: true,
-    url: "http://localhost:4000/login",
-  }).then((res) => console.log(res));
-};
-const getUser = () => {
-  Axios({
-    method: "GET",
-    withCredentials: true,
-    url: "http://localhost:4000/user",
-  }).then((res) => {
-    setData(res.data);
-    console.log(res.data);
-  });
-};
-return (
-  <div className="App">
-    <div>
-      <h1>Register</h1>
-      <input
-        placeholder="username"
-        onChange={(e) => setRegisterUsername(e.target.value)}
-      />
-      <input
-        placeholder="password"
-        onChange={(e) => setRegisterPassword(e.target.value)}
-      />
-      <button onClick={register}>Submit</button>
-    </div>
-
-    <div>
-      <h1>Login</h1>
-      <input
-        placeholder="username"
-        onChange={(e) => setLoginUsername(e.target.value)}
-      />
-      <input
-        placeholder="password"
-        onChange={(e) => setLoginPassword(e.target.value)}
-      />
-      <button onClick={login}>Submit</button>
-    </div>
-
-    <div>
-      <h1>Get User</h1>
-      <button onClick={getUser}>Submit</button>
-      {data ? <h1>Welcome Back {data.username}</h1> : null}
-    </div>
-  </div>
-);
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003';
+}
+else {
+  baseURL = 'your heroku backend url here';
 }
 
-    </div>
-  );
+export default class App extends Component {
+ constructor(props) {
+   super(props)
+   this.state = {
+     username: '',
+     password: '',
+     email: '',
+     pattern: '',
+   }
+ }
+ render () {
+    return (
+      <>
+        Hello
+        <NewUser baseURL={baseURL} />
+        <UpdateUser baseURL={baseURL} currentUser="60785e9f66836507fcba78cd"/>
+        <DeleteUser baseURL={baseURL} currentUser="60789d84826c565a8459ed9b"/>
+        <JournalUser baseURL={baseURL} currentUser="60785e9f66836507fcba78cd"/>
+        <ShowUsers baseURL={baseURL} />
+        <ShowSingleUser baseURL={baseURL} currentUser="60785e9f66836507fcba78cd"/>
+        <JournalShow baseURL={baseURL} currentUser="60785e9f66836507fcba78cd" datestamp="2021-04-15T21:33:44.882Z"/>
+        <JournalEdit baseURL={baseURL} currentUser="60785e9f66836507fcba78cd" datestamp="2021-04-15T21:33:44.882Z"/>
+      </>
+   );
+  }
 }
-
-export default App;
