4509ac0b7e304ccb20ab7e0190c2c4c1aa21c3e9
Stephen_Price <STEPHEN.WESLEY.PRICE.COM>
2021-04-17 14:45:11 -0400

    select/option is working to fetch area code

--- birdwatching_fe/src/App.js
+++ birdwatching_fe/src/App.js
@@ -11,57 +11,84 @@ export default class App extends Component {
    super(props)
 
    this.state = {
-      // areaCode: ""
-      // US state user selects
+    areaCode: 'VA',
+    baseUrl: 'https://api.ebird.org/v2/data/obs/US-',
+    recent: '/recent?maxResults=1'
+      
    }
   }  
 
-  componentDidMount() {
-    this.getBird()
+  // componentDidMount() {
+  //   this.getBird()
+  // }
+  handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState({[event.target.id] : event.target.value})
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.getBird()
+  }
 
   async getBird() {
     // npm install axios on "birdwatch_fe"
     // axios.get(`https://api.ebird.org/v2/data/obs/US-${VA}/recent`
-    await axios.get('https://api.ebird.org/v2/data/obs/US-NY-109/recent', { 
+    await axios.get(this.state.baseUrl + this.state.areaCode + this.state.recent, { 
     headers: {
       'X-eBirdApiToken': 'sd1dnat8ktfu'
+      
+    
+      // 'maxResults': 1
     }
   }).then(res => {
     console.log(res.data)
     this.setState({birds: res.data})
   })
-  }
+
   
+  }
+  handleSubmit() {
+    // search the birdlist bird.comName
+  }
  
 
   render () {
     let birdlist;
     this.state.birds &&
-    (birdlist = this.state.birds.map((bird, id) => {
+    (birdlist = this.state.birds.map((bird, id) => { 
       return (
         <div key={id}>
-          <p>
-            {bird.comName}
-            {bird.locName}
-            {bird.obsDt}
-            {bird.sciName}
-          </p>
+         
+            <ul>Common Name:{bird.comName}
+            <li>Location Seen: {bird.locName}</li>
+            <li>Observed Date: {bird.obsDt}</li>
+            <li>Scientific Name: {bird.sciName}</li>
+            </ul>
+    
         </div>
-//         comName: "Carolina Wren"
-// locName: "Dryden Lake"
-// obsDt: "2021-04-16 10:37"
-// sciName: "Thryothorus ludovicianus"
+
       )}
       ))
     return (
       <div className="App">
-        <h1>Bird APP</h1>
-        {birdlist}
-        {/* <BirdData/> */}
+        
+        <form onSubmit={this.handleSubmit}>
+        <label htmlFor='areaCode'>Area Code</label>
+        <select onChange={this.handleChange} value={this.state.areaCode} id="areaCode" name="Area Code">
+          <option value='VA'>VA</option>
+          <option value='NY'>NY</option>
+        </select>
+       
+        <input 
+          type='submit' 
+          value='Find Birds with Area Code'>
+        </input>
+      </form>
+      {birdlist}
       </div>
     );
   }
 }
+
 
